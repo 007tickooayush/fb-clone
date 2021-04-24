e1c8d05bbd7771eb48fa72fadac8ced9fda07672
007tickooayush <ayush.tickoo292000@gmail.com>
2021-04-24 12:45:07 +0530

    guard and other components initialized

--- FB/src/app/app-routing.module.ts
+++ FB/src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
+/* tslint:disable:whitespace no-trailing-whitespace */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Router, RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {HomeGuard} from './guards/home.guard';
 
-
+const routes: Routes = [
+  {
+      path:'', loadChildren:() => (import('./components/home/home.module').then(m => m.HomeModule)), // lazy loading
+    canActivate:[HomeGuard]
+  },
+  {
+    path:'login',component:LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
--- FB/src/app/app.module.ts
+++ FB/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import {AppRoutingModule} from './app-routing.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
