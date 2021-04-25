65ffe5627878723c2cb5ba4dcae2f6e471fc94b4
007tickooayush <ayush.tickoo292000@gmail.com>
2021-04-25 09:25:46 +0530

    components created

--- fb-clone/src/app/shared/material.module.ts
+++ fb-clone/src/app/shared/material.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
 
-
+const modules: any[] = [
+  MatIconModule,
+  MatCardModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatDialogModule
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ...modules
+  ],
+  exports: [
+    ...modules
   ]
 })
 export class MaterialModule { }
+// token : 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
